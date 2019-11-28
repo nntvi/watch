91da6cbd1f52124378a69629c536d811e409d8f8
nntvi <nntvi.2609@gmail.com>
2019-11-28 22:39:01 +0700

    second commit

--- admin/modules/sale/controllers/indexController.php
+++ admin/modules/sale/controllers/indexController.php
@@ -1,31 +1,72 @@
 <?php
-    function construct()
-    {
-        load_model('index');
-    }
-    
-    function list_customerAction(){
-        $data['customer'] = get_list_customer();
-        load_view('list_customer',$data);
-    }
+function construct()
+{
+    load_model('index');
+}
 
-    function list_orderAction(){
-        $data['order'] = get_list_order();
-        load_view('list_order',$data);
-    }
+function list_customerAction()
+{
+    $data['customer'] = get_list_customer();
+    load_view('list_customer', $data);
+}
 
-    function detail_orderAction(){
-        $id = $_GET['id'];
-        if(isset($_POST['btn_update_status'])){
-            $data = array(
-                'status' => $_POST['status']
-            );
-            update_status($data,$id);
-            redirect("?mod=sale&action=list_order");
+function list_orderAction()
+{
+    $num_rows = db_num_rows("SELECT tbl_orders.order_id,cus_name,detail_qty,order_sub_total,
+        cus_date, count(tbl_detail_order.order_id) sl,
+        status FROM tbl_orders 
+        INNER JOIN tbl_customers on tbl_orders.cus_id = tbl_customers.cus_id 
+        INNER JOIN tbl_detail_order on tbl_orders.cus_id = tbl_detail_order.cus_id 
+        GROUP BY tbl_orders.order_id, tbl_customers.cus_name");
+
+    # Số lượng bản ghi trên trang
+    $num_per_page = 3;
+    $total_row = $num_rows;
+
+    #Tính tổng số trang
+    $num_page = ceil($total_row / $num_per_page);
+    $page = isset($_GET['page']) ? (int) $_GET['page'] : 1;
+    $start = ($page - 1) * $num_per_page;
+
+    $list_order = get_order($start,$num_per_page);
+  
+    $data['page'] = $page;
+    $data['num_page'] = $num_page;
+    $data['order'] = $list_order;
+    load_view('list_order', $data);
+}
+
+function detail_orderAction()
+{
+    $id = $_GET['id'];
+    if (isset($_POST['btn_update_status'])) {
+        $data = array(
+            'status' => $_POST['status']
+        );
+        update_status($data, $id);
+        redirect("?mod=sale&action=list_order");
+    }
+    $data['detail_order'] = get_detail_order($id);
+    $data['dt_od_product'] = get_detail_order_product($id);
+    $data['result_order'] = get_result_order(($id));
+    load_view('detail_order', $data);
+}
+
+function searchAction(){
+    global $error, $str;
+    if(isset($_POST['btn_search'])){
+        $str = '';
+        $error = array();
+        if(empty($_POST['search'])){
+            $error['search'] = "Nhập từ khoá tìm kiếm";
+        }else{
+            $str = $_POST['search'];
         }
-        $data['detail_order'] = get_detail_order($id);
-        $data['dt_od_product'] = get_detail_order_product($id);
-        $data['result_order'] = get_result_order(($id));
-        load_view('detail_order',$data);
     }
-?>
+    // nếu tìm kiếm thấy có trong bảng
+    if(result_search($str) > 0){ 
+        $list_search = get_search($str);
+    }
+    $data['list_search'] = $list_search;
+    load_view('search',$data);
+}
--- admin/modules/sale/models/indexModel.php
+++ admin/modules/sale/models/indexModel.php
@@ -13,7 +13,16 @@ function get_list_order()
                                         INNER JOIN tbl_detail_order on tbl_orders.cus_id = tbl_detail_order.cus_id 
                                         GROUP BY tbl_orders.order_id, tbl_customers.cus_name");
 }
-
+#Hàm xử lý show toàn bộ thông tin và xử lý phân trang
+function get_order($start = 1, $num_per_page = 10, $where = "")
+{
+    $list_order = db_fetch_array("SELECT tbl_orders.order_id,cus_name,detail_qty,order_sub_total,
+    cus_date, count(tbl_detail_order.order_id) sl, status FROM tbl_orders 
+    INNER JOIN tbl_customers on tbl_orders.cus_id = tbl_customers.cus_id 
+    INNER JOIN tbl_detail_order on tbl_orders.cus_id = tbl_detail_order.cus_id 
+    GROUP BY tbl_orders.order_id, tbl_customers.cus_name LIMIT {$start}, {$num_per_page}");
+    return $list_order;
+}
 function get_detail_order($id)
 {
     return db_fetch_row(("SELECT order_method, status, cus_address, tbl_orders.order_id FROM tbl_customers inner join tbl_orders on tbl_orders.cus_id = tbl_customers.cus_id WHERE tbl_orders.order_id = $id"));
@@ -31,13 +40,54 @@ function get_detail_order_product($id)
          tbl_products ON tbl_detail_order.pro_id = tbl_products.pro_id
                                     where tbl_orders.order_id = $id");
 }
-function get_result_order($id){
+function get_result_order($id)
+{
     return db_fetch_row("SELECT SUM(detail_qty) as sum_sl, order_sub_total 
                             FROM tbl_orders INNER JOIN tbl_detail_order 
                             on tbl_orders.order_id = tbl_detail_order.order_id
                             WHERE tbl_orders.order_id = $id
                             GROUP BY tbl_orders.order_id");
 }
-function update_status($data,$order_id){
-    db_update('tbl_orders',$data,"`order_id` = '{$order_id}'");
+function update_status($data, $order_id)
+{
+    db_update('tbl_orders', $data, "`order_id` = '{$order_id}'");
+}
+
+function get_pagging($num_page, $page, $base_url = "")
+{
+    $str_pagging = "<ul class='pagging'>";
+    if ($page > 1) {
+        $page_prev = $page - 1;
+        $str_pagging .= "<li><a href = \"{$base_url}&page={$page_prev}\">Trước</a></li>";
+    }
+    for ($i = 1; $i <= $num_page; $i++) {
+        $active = "";
+        if ($i == $page) {
+            $active = "class = 'active'";
+        }
+        $str_pagging .= "<li {$active}><a href=\"{$base_url}&page={$i}\">{$i}</a></li>";
+    }
+    if ($page < $num_page) {
+        $page_next = $page + 1;
+        $str_pagging .= "<li><a href=\"{$base_url}&page={$page_next}\">Sau</a></li>";
+    }
+    $str_pagging .= "</ul>";
+    return $str_pagging;
+}
+
+#Xử lí tìm kiếm
+function result_search($str){
+    $item = db_num_rows("SELECT * FROM `tbl_customers` WHERE `cus_name` LIKE '%{$str}%'");
+    return $item;
+}
+
+function get_search($str)
+{
+    $list_order = db_fetch_array("SELECT tbl_orders.order_id,cus_name,detail_qty,order_sub_total,
+    cus_date, count(tbl_detail_order.order_id) sl,
+    status FROM tbl_orders 
+    INNER JOIN tbl_customers on tbl_orders.cus_id = tbl_customers.cus_id 
+    INNER JOIN tbl_detail_order on tbl_orders.cus_id = tbl_detail_order.cus_id 
+    GROUP BY tbl_orders.order_id, tbl_customers.cus_name where cus_name like '%{$str}%'");
+    return $list_order;
 }
